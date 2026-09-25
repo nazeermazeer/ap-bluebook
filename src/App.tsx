81788ae8581c7a860d19a7f7a549e55a7e35e430
nazeermazeer <nazeerak255@outlook.com>
2026-09-25 15:50:02 -0600

    add ap gov question

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <main className="container">
-        <button>This is a button</button>
+        <h1>Which of the following ideals of democracy is reflected in the procedure for apportionment in the House of Representatives?</h1>
+        <button>The federal government represents states equally.</button>
+        <button>Government is based on the consent of the governed.</button>
+        <button>Leaders in Congress are among the most well-informed in society.</button>
+        <button>Political power is distributed between the national government and state governments.</button>
     </main>
   );
 }
