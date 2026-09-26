e20ee2cf8c5b79cfc962a53eeb32cd9d315cd436
nazeermazeer <nazeerak255@outlook.com>
2026-09-25 18:50:45 -0600

    switch from heading to paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <main className="container">
-        <h1>Which of the following ideals of democracy is reflected in the procedure for apportionment in the House of Representatives?</h1>
+        <p>Which of the following ideals of democracy is reflected in the procedure for apportionment in the House of Representatives?</p>
         <button>The federal government represents states equally.</button>
         <button>Government is based on the consent of the governed.</button>
         <button>Leaders in Congress are among the most well-informed in society.</button>
