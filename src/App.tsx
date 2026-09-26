906ad3b02112884a0e6ceb31fd4be57f63e06389
nazeermazeer <nazeerak255@outlook.com>
2026-09-26 07:53:46 -0600

    center everything horizontally

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-        <p>Hierarchical diffusion is defined as the movement of a culture trait</p>
-        <button>across a geographic barrier such as an ocean or desert</button>
-        <button>from a hearth or place of origin outward in all directions</button>
-        <button>from more influential places to less influential places</button>
-        <button>between places that are in close proximity to a line of transport</button>
-        <button>that affects all places simultaneously regardless of their location</button>
+    <main>
+        <div className="questions">
+            <div>
+                <p>Hierarchical diffusion is defined as the movement of a culture trait</p>
+                <button>across a geographic barrier such as an ocean or desert</button>
+                <button>from a hearth or place of origin outward in all directions</button>
+                <button>from more influential places to less influential places</button>
+                <button>between places that are in close proximity to a line of transport</button>
+                <button>that affects all places simultaneously regardless of their location</button>
+            </div>
+        </div>
     </main>
   );
 }
