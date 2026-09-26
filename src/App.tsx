2825e20c1952cdff7c7b6dc34f3942524ef1d73e
nazeermazeer <nazeerak255@outlook.com>
2026-09-26 11:21:50 -0600

    cleanup as i adjust alignment offset of questions

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ function App() {
   return (
     <main>
         <div className="questions">
-            <div>
-                <p>Hierarchical diffusion is defined as the movement of a culture trait</p>
-                <button>across a geographic barrier such as an ocean or desert</button>
-                <button>from a hearth or place of origin outward in all directions</button>
-                <button>from more influential places to less influential places</button>
-                <button>between places that are in close proximity to a line of transport</button>
-                <button>that affects all places simultaneously regardless of their location</button>
-            </div>
+            <p>Hierarchical diffusion is defined as the movement of a culture trait</p>
+            <button>across a geographic barrier such as an ocean or desert</button>
+            <button>from a hearth or place of origin outward in all directions</button>
+            <button>from more influential places to less influential places</button>
+            <button>between places that are in close proximity to a line of transport</button>
+            <button>that affects all places simultaneously regardless of their location</button>
         </div>
     </main>
   );
