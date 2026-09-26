4f1bbe805b057e5ebbe15785b313e9a2c503b3bd
nazeermazeer <nazeerak255@outlook.com>
2026-09-26 07:44:48 -0600

    further button adjustments + change the question

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 function App() {
   return (
     <main className="container">
-        <p>Which of the following ideals of democracy is reflected in the procedure for apportionment in the House of Representatives?</p>
-        <button>The federal government represents states equally.</button>
-        <button>Government is based on the consent of the governed.</button>
-        <button>Leaders in Congress are among the most well-informed in society.</button>
-        <button>Political power is distributed between the national government and state governments.</button>
+        <p>Hierarchical diffusion is defined as the movement of a culture trait</p>
+        <button>across a geographic barrier such as an ocean or desert</button>
+        <button>from a hearth or place of origin outward in all directions</button>
+        <button>from more influential places to less influential places</button>
+        <button>that affects all places simultaneously regardless of their location</button>
     </main>
   );
 }
